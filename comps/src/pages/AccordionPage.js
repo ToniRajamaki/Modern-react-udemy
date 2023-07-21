1e01186c34c01160fa75c6bc6b9ce46e1f16bbee
TONI <zingoo16@gmail.com>
2023-07-21 12:41:26 +0300

    modified sample data in accordion project

---
+++ comps/src/pages/AccordionPage.js
@@ -0,0 +1,29 @@
+import Accordion from '../components/Accordion'
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'asd1',
+      label: 'What is the meaning of life?',
+      content: 'According to some people its #42',
+    },
+    {
+      id: 'asd3',
+      label: 'What is the fundamental rule of learning?',
+      content: 'You have to apply your knowledge.',
+    },
+    {
+      id: 'asd2',
+      label: 'How many questions there are in total?',
+      content: 'There is three questions. Or is there?',
+    },
+  ]
+
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
+}
+
+export default AccordionPage
