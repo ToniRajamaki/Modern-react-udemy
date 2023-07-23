04e07568ff764ffd723d1e27a5933fdc4479e54e
TONI <zingoo16@gmail.com>
2023-07-23 12:05:36 +0300

    added sidebar for navigation

--- comps/src/App.js
+++ comps/src/App.js
@@ -2,18 +2,22 @@ import Link from './components/Link'
 import Route from './components/Route'
 import AccordionPage from './pages/AccordionPage'
 import DropdownPage from './pages/DropdownPage'
+import SideBar from './components/Sidebar'
+import ButtonPage from './pages/ButtonPage'
 
 function App() {
   return (
-    <div>
-      <Link to='/accordion'>Go To Accordion</Link>
-      <Link to='/dropdown'>Go To dropdown</Link>
-      <div>
+    <div className='container mx-atuo grid grid-cols-6 gap-4 nt-4'>
+      <SideBar />
+      <div className='col-span-5'>
         <Route path='/accordion' exact>
           <AccordionPage />
         </Route>
-        <Route path='/dropdown' exact>
+        <Route path='/' exact>
           <DropdownPage />
+        </Route>
+        <Route path='/buttons' exact>
+          <ButtonPage />
         </Route>
       </div>
     </div>
