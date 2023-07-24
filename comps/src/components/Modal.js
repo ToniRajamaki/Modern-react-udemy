20fda288795fe8925b77cc7bc9bfecaf4959d37d
TONI <zingoo16@gmail.com>
2023-07-24 11:38:12 +0300

    module now has somewhat working functionality

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -3,11 +3,17 @@ import Button from './Button'
 import Panel from './Panel'
 import ReactDOM from 'react-dom'
 
-function Modal() {
+function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
-      <div className='absolute inset-0 bg-gray-500 opacity-80'></div>
-      <div className='absolute inset-40 p-10 bg-white'> Im a modal</div>
+      <div
+        onClick={onClose}
+        className='absolute inset-0 bg-gray-500 opacity-80'
+      ></div>
+      <div className='absolute inset-40 p-10 bg-white'>
+        {children}
+        {actionBar}
+      </div>
     </div>,
     document.querySelector('.modal-container')
   )
--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -5,18 +5,31 @@ import Button from '../components/Button'
 
 function ModalPage() {
   const [showModal, SetShowModal] = useState(false)
-  const onClose = () => {
+  const handleClose = () => {
     SetShowModal(false)
   }
   const handleClick = () => {
     SetShowModal(true)
   }
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  )
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is some really important thing you should know!</p>
+    </Modal>
+  )
   return (
     <div>
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal onClose={onClose}></Modal>}
+      {showModal && modal}
     </div>
   )
 }
